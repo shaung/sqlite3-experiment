3246b484aa74377ceec32a653c5a3db1bdff67fc
shaung <shaun.geng@gmail.com>
2012-07-16 11:45:31 +0900

    tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -48,7 +48,21 @@ class TestBookStore:
         result = self.db.select("select t.id from jsondata t where t.id in (4,9,14,20) and exists (select * from (select id, type, value from jsondata t0 where t0.parent = (select id from jsondata where type = 8 and parent = t.id and value = 'author') union all select -9, -1, NULL) __t0__ where (__t0__.type > 0 and __t0__.value is not NULL and __t0__.value = 'Evelyn Waugh') and (__t0__.type > 0))")
         eq_([row['id'] for row in result], [9])
 
-    def test_travis_eq2(self):
+    def test_internal_eq_try(self):
+        result = self.db.select("""
+        select t.id from jsondata t
+        where t.id in (4,9,14,20) and exists (
+            select * from (
+                select id, type, value from jsondata
+                where parent = (select id from jsondata where type = 8 and parent = t.id and value = 'author')
+                union all
+                select -9, -1, ''
+            ) __t0__
+            where __t0__.type > 0 and __t0__.value = 'Evelyn Waugh' and __t0__.type > 0)""")
+        eq_([row['id'] for row in result], [9])
+
+
+    def test_travis_eq_pass(self):
         result = self.db.select("""
         select id, type, value from (
             select id, type, value from jsondata t 
@@ -60,7 +74,7 @@ class TestBookStore:
         eq_([row['id'] for row in result], [37])
         eq_([row['value'] for row in result], ['Evelyn Waugh'])
 
-    def test_travis_eq3(self):
+    def test_travis_eq_pass2(self):
         result = self.db.select("""
         select id, type, value from jsondata t where t.value = 'Evelyn Waugh'
         """)
